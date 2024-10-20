07425ab8b086efd425db4422c18aac11796bbbb2
genuinoflavio <genuinoflavio@gmail.com>
2024-10-20 16:24:20 -0300

    Criação do APP cars no projeto DJANGO

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #USER DEFINED APPS
+    'cars',
 ]
 
 MIDDLEWARE = [
