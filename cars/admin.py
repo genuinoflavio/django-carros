b0d63c016a26b4a36ce5b734776f7be5a554e181
genuinoflavio <genuinoflavio@gmail.com>
2024-10-20 19:25:23 -0300

    Config  app car + brand + image upload

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Car, Brand
 
 # Register your models here.
+class BrandAdmin(admin.ModelAdmin): 
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'model_year','brand', 'price')
+    search_fields = ('model',)
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
